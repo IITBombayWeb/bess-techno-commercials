05de3da91bb58219b5d0893f5ffc4b982e555e8f
P Sunthar <p.sunthar@gmail.com>
2024-06-06 01:07:24 +0530

    estimate soc0 by one iteration

--- src/solarBESSshifts.py
+++ src/solarBESSshifts.py
@@ -2,40 +2,12 @@ import numpy as np
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 
-
 # sampling time in hours
 Tsamp = 1/12
-Rn = 70 # normal rate of grid cost, Rs/Unit
-
-# Load demand
-Tlavg = 50 # kW average load demand
-# list of periods when there is a load demand at the average value
-Tllist = [(0, 3),
-          (6.5, 16),
-          (17,24)]
-
-# Solar parameters
-Pmax = 100 # Solar capacity in kW
-
-# Simple quad model (trapezium profile) for solar production
-# start time, max start, max end, end 
-Tsquad = (7, 10, 15, 18.5)
-
-# Grid slabs
-# List of Normal rate periods
-Tgnlist = [(10,18)]
-
-# List of Peak rate periods
-Tgplist = [(6, 10), (18, 22)] 
-Rp = Rn*1.25
-
-# List of off-peak rate periods
-Tgolist = [(0, 6), (22, 24)]
-Ro = Rn*0.75
 
 # BESS parameters
 # Capacity of the battery in kWh
-Cbess = 200
+Cbess = 300
 
 # C-Rating
 Cr = 0.5
@@ -45,6 +17,39 @@ SoCmax = 90
 
 # State of Charge (min)
 SoCmin = 10
+
+# Load demand
+Plmax = 75 # kW max contracted load
+Tlavg = 60 # kW average load demand
+
+# list of periods when there is a load demand at the average value
+Tllist = [(0, 3),
+          (6.5, 16),
+          (17,24)]
+
+# Solar parameters
+Psmax = 100 # Solar capacity in kW
+
+# Simple quad model (trapezium profile) for solar production
+# start time, max start, max end, end 
+Tsquad = (8, 11, 15, 17.5)
+
+# Grid slabs
+# List of Normal rate periods
+Tgnlist = [(10,18)]
+
+Rn = 70 # normal rate of grid cost, Rs/Unit
+
+
+
+# List of Peak rate periods
+Tgplist = [(6, 10), (18, 22)] 
+Rp = Rn*1.25
+
+# List of off-peak rate periods
+Tgolist = [(0, 6), (22, 24)]
+Ro = Rn*0.75
+
 
 
 
@@ -132,14 +137,17 @@ def grid_rates(Tgnlist, Tgplist, Tgolist, Rn, Rp, Ro):
 
     return Rgrid
 
-def battery_state(Pload, Psolar, Cr, SoCmax, SoCmin, 
+def isem_control(Pload, Psolar, Plmax, Cbess, Cr, SoC0, SoCmax, SoCmin, 
                   Tgnlist, Tgplist, Tgolist, ts):
     """ Returns battery SoC and Grid power used
 
     Args:
         Pload (numpy array): time series of load demand
         Psolar (numpy array): time series of solar power production
+        Plmax (float): Max contracted load
+        Cbess (float): Capacity of the battery in kWh
         Cr (float): C-Rating of the battery
+        SoC0 (float): initial state of charge
         SoCmax (float): maximum state of charge
         SoCmin (float): minimum state of charge
         Tgnlist (list): list of Normal rate periods
@@ -154,8 +162,6 @@ def battery_state(Pload, Psolar, Cr, SoCmax, SoCmin,
 
     tsamp = ts[1] - ts[0]
 
-        #Cbess (float): Capacity of the battery in kWh
-    Cbess = 215
     SoC = 0 * ts # initialise all to zero
     Pgrid = 0*ts # initialise all to zero
 
@@ -164,7 +170,9 @@ def battery_state(Pload, Psolar, Cr, SoCmax, SoCmin,
     for i, t in enumerate(ts):
 
         if (i==0):
-            soc = SoCmax
+            soc = SoC0
+            # soc = 25 # 50 kW avg
+            # soc = 34 # 50 kW avg
         else:
             soc = SoC[i-1]
 
@@ -178,17 +186,18 @@ def battery_state(Pload, Psolar, Cr, SoCmax, SoCmin,
             # Pgrid[i] = 0
 
         else:
-            # Off peak rates, grid charging
+            # Off peak rates, excess grid charging
             for tb,tn in Tgolist:
                 if ((t>= tb) and (t<=tn)):
                     # print('Off peak charging')
-                    SoC[i] = np.min([soc + (100*Cr) * tsamp, SoCmax])
+                    SoC[i] = np.min([soc + (100*Cr) * tsamp * (Plmax+Pnet)/(Cbess*Cr), SoCmax])
+                    # note Pnet is negative
 
-            # Normal rates, grid charging
+            # Normal rates, excess grid charging
             for tb,tn in Tgnlist:
                 if ((t>= tb) and (t<=tn)):
                     # print('Normal charging')
-                    SoC[i] = np.min([ soc + (100*Cr) * tsamp, SoCmax])
+                    SoC[i] = np.min([ soc + (100*Cr) * tsamp * (Plmax+Pnet)/(Cbess*Cr), SoCmax])
 
             # Peak rates, grid discharging
             if (soc >= SoCmin) : # support load during peak time
@@ -202,8 +211,8 @@ def battery_state(Pload, Psolar, Cr, SoCmax, SoCmin,
                 print('Battery empty')
                 SoC[i] = soc
 
-        if (Pnet < 0):
-            Pgrid[i] = np.max([-Pnet + (Cbess*Cr) * (SoC[i] - soc) / tsamp, 0])
+        if (Pnet <= eps):
+            Pgrid[i] = np.max([-Pnet + Cbess * (SoC[i] - soc) / (100 * tsamp), 0])
             print(f"Pgrid[i] = {Pgrid[i]}")
 
         print(f"t={t}, Pnet={Pnet}, SoC={SoC[i]}")
@@ -215,18 +224,22 @@ def battery_state(Pload, Psolar, Cr, SoCmax, SoCmin,
 ts = np.arange(0, 24+Tsamp, Tsamp)
 
 Pload = load_demand(Tlavg, Tllist, ts)
-Psolar = solar_power(Pmax, Tsquad, ts)
+Psolar = solar_power(Psmax, Tsquad, ts)
 
 Rgrid = grid_rates(Tgnlist, Tgplist, Tgolist, Rn, Rp, Ro)
 
-SoC,Pgrid = battery_state(Pload, Psolar, Cr, SoCmax, SoCmin, Tgnlist, Tgplist, Tgolist, ts)
+SoC0 = 50 # initial guess
+SoC,Pgrid = isem_control(Pload, Psolar, Plmax, Cbess, Cr, SoC0, SoCmax, SoCmin, Tgnlist, Tgplist, Tgolist, ts)
+SoC0 = SoC[-1] # iterate with 24 hr value
+SoC,Pgrid = isem_control(Pload, Psolar, Plmax, Cbess, Cr, SoC0, SoCmax, SoCmin, Tgnlist, Tgplist, Tgolist, ts)
+
 
 
 plt.plot(ts, Pload, label='Load Power', ls='-', color='b')
 plt.plot(ts, Psolar, label='Solar Power', ls='-', color='r')
-plt.plot(ts, Rgrid, label='Grid Rate', ls='-', color='m')
+plt.plot(ts, Pgrid, label='Grid Power', ls='-', marker='o', color='k')
+plt.plot(ts, Rgrid, label='Grid Cost [Rs/10 kWh]', ls='-', color='m')
 plt.plot(ts, SoC, label='SoC', ls='-', marker='o', color='g')
-#plt.plot(ts, Pgrid, label='Grid Power', ls='-', marker='o', color='k')
 
 #plt.plot(ts, Psolar, label='Solar Power', ls='-', marker='o', color='r')
 
